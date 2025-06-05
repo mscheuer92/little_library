ff584e07dcb10a3f76197af97b28a529f78e430b
Michelle <michelle@Michelles-MacBook-Air.local>
2025-06-05 09:29:50 -0400

    optomize functions

--- booktracker/bookshelf/views.py
+++ booktracker/bookshelf/views.py
@@ -7,41 +7,40 @@ from bookshelf.api.serializers import BookSerializer
 #/book/list/
 @api_view(['GET','POST'])
 def get_book_list(request):
-    if request.method == 'GET':
-        book = Book.objects.all()
-        # Use serializer
-        serializer = BookSerializer(book, many=True)
-        return Response(serializer.data)
-    elif request.method == 'POST':
-        serializer = BookSerializer(data=request.data) # get data from user request
-        if serializer.is_valid(): # data is considered valid if it's in the correct format
-            serializer.save()
-            return Response(serializer.data, status=201)
-        else:
-            return Response(serializer.errors)
+    match request.menthod:
+        case 'GET':
+            book = Book.objects.all()
+            # Use serializer
+            serializer = BookSerializer(book, many=True)
+            return Response(serializer.data)
+        case 'POST':
+            serializer = BookSerializer(data=request.data) # get data from user request
+            if serializer.is_valid(): # data is considered valid if it's in the correct format
+                serializer.save()
+                return Response(serializer.data, status=201)
+            else:
+                return Response(serializer.errors, status=404)
     
-
 # View for getting a specific book by id.
 #/book/<id>
 @api_view(['GET','PUT','DELETE'])
 def get_book_by_id(request, pk):
-    match request.method:
-        case 'GET':
-            book = Book.objects.get(pk=pk)
-            serializer=BookSerializer(book)
-            return Response(serializer.data)
-        case 'PUT':
-            book = Book.objects.get(pk=pk)
-            serializer = BookSerializer(book, data=request.data)
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data, status=200)
-            else:
-                return Response(serializer.errors, status=400)
-        case 'DELETE':
-            book = Book.objects.get(pk=pk)
-            book.delete()
-            return Response(status=204)
+    try:
+        book = Book.objects.get(pk=pk)
+        match request.method:
+            case 'GET':
+                serializer=BookSerializer(book)
+                return Response(serializer.data, status=200)   
+            case 'PUT':
+                serializer = BookSerializer(book, data=request.data)
+                if serializer.is_valid():
+                    serializer.save()
+                    return Response(serializer.data, status=200)
+            case 'DELETE': 
+                book.delete()
+                return Response(status=204)
+    except Book.DoesNotExist:
+        return Response(status=404)
 
 #/book/title/title/
 @api_view()
