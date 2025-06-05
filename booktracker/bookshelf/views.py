84d10eef2e6b565455a6be1a9124c638b29ad948
Michelle <michelle@Michelles-MacBook-Air.local>
2025-06-05 09:42:35 -0400

    add validation and status codes

--- booktracker/bookshelf/views.py
+++ booktracker/bookshelf/views.py
@@ -12,14 +12,14 @@ def get_book_list(request):
             book = Book.objects.all()
             # Use serializer
             serializer = BookSerializer(book, many=True)
-            return Response(serializer.data)
+            return Response(serializer.data, status=200)
         case 'POST':
             serializer = BookSerializer(data=request.data) # get data from user request
             if serializer.is_valid(): # data is considered valid if it's in the correct format
                 serializer.save()
                 return Response(serializer.data, status=201)
             else:
-                return Response(serializer.errors, status=404)
+                return Response(serializer.errors, status=400)
     
 # View for getting a specific book by id.
 #/book/<id>
@@ -36,22 +36,30 @@ def get_book_by_id(request, pk):
                 if serializer.is_valid():
                     serializer.save()
                     return Response(serializer.data, status=200)
+                else:
+                    return Response(serializer.errors, status=400)
             case 'DELETE': 
                 book.delete()
                 return Response(status=204)
     except Book.DoesNotExist:
-        return Response(status=404)
+        return Response({"error": "Book not found"}, status=404)
 
 #/book/title/title/
 @api_view()
 def get_book_by_title(request, title):
-    book = Book.objects.get(title=title)
-    serializer = BookSerializer(book)
-    return Response(serializer.data)
+    try:
+        book = Book.objects.get(title=title)
+        serializer = BookSerializer(book)
+        return Response(serializer.data)
+    except Book.DoesNotExist:
+        return Response({"error": "Title not found"}, status=404)
 
 # list all books by specific author
 @api_view()
 def get_books_by_author(request, author):
-    book = Book.objects.filter(author=author)
-    serializer = BookSerializer(book, many=True)
-    return Response(serializer.data)
+    try:
+        book = Book.objects.filter(author=author)
+        serializer = BookSerializer(book, many=True)
+        return Response(serializer.data)
+    except Book.DoesNotExist:
+        return Response({"error": "Author not found"}, status=404)
